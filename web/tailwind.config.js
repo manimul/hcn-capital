85de567c7cd98b03c719b38af0739eb448af64e8
Mark Mulvaney <mark@bambwa.com>
2023-02-10 16:00:25 +0100

    downgraded tailwind v

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,9 +1,12 @@
 module.exports = {
   content: [
     './pages/**/*.js',
+    './pages/*.js',
     './components/sections/**/*.js',
+    './components/sections/*.js',
     './pages/sections/**/*.js',
     './components/**/*.js',
+    './components/*.js',
   ],
   theme: {
     extend: {},
