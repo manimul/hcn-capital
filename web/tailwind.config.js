45644b4193dcf363d72782ed4577d9fc0bb723ad
Mark Mulvaney <mark@bambwa.com>
2023-02-10 14:53:36 +0100

    changed to tailwind config to fix prod css issue

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './pages/sections/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.js',
+    './components/sections/**/*.js',
+    './pages/sections/**/*.js',
+    './components/**/*.js',
   ],
   theme: {
     extend: {},
